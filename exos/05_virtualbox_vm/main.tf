e716ecfe69ba8509089e3d6e93eecdf3d552af61
Romain CORDIER <r.cordier@ecole-ipssi.net>
2025-10-08 12:11:01 +0200

    Exercice 5.1  Créer une VM VirtualBox

--- exos/05_virtualbox_vm/main.tf
+++ exos/05_virtualbox_vm/main.tf
@@ -1,28 +1,19 @@
 terraform {
   required_providers {
     virtualbox = {
-      source  = "shekeriev/virtualbox"
-      version = "0.0.4"
+      source  = "terra-farm/virtualbox"
+      version = "0.2.2-alpha.1"
     }
   }
 }
 
-provider "virtualbox" {
-  delay      = 60
-  mintimeout = 5
-}
+provider "virtualbox" {}
 
 resource "virtualbox_vm" "ubuntu_vm" {
   name   = "ubuntu_vm"
-
-  # Chemin local vers ton image Bento Ubuntu 22.04 téléchargée
-  # (tu peux l’adapter selon ton dossier)
-  image  = "C:\\images\\bento-ubuntu-22.04-202508.03.0.box"
-
+  image  = "${path.module}/images/bento-ubuntu-22.04-amd64.box"
   cpus   = 1
   memory = "1024 mib"
 
-  network_adapter {
-    type = "nat"
-  }
+  network_adapter { type = "nat" }
 }
