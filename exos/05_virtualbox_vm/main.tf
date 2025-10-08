f21c6a5638ec9015485fc035bbe8001ff1ea7a2a
Romain CORDIER <r.cordier@ecole-ipssi.net>
2025-10-08 10:39:35 +0200

    Exercice 5.1  Créer une VM VirtualBox

--- exos/05_virtualbox_vm/main.tf
+++ exos/05_virtualbox_vm/main.tf
@@ -1,21 +1,21 @@
-terraform {
+terraform { 
   required_providers {
-    virtualbox = {
-      source  = "terra-farm/virtualbox"
-      version = "0.2.2-alpha.1"
+     virtualbox = {
+       source = "shekeriev/virtualbox"
+       version = "0.0.4"
+       }
     }
-  }
-}
+} 
 
-provider "virtualbox" {}
+provider "virtualbox" {
+  delay = 60
+  mintimeout = 5
+} 
 
 resource "virtualbox_vm" "ubuntu_vm" {
-  name   = "ubuntu_vm"
-  image  = "C:\\images\\jammy-server-cloudimg-amd64.ova"
-  cpus   = 1
-  memory = "1024 mib"
-
-  network_adapter {
-    type = "nat"
-  }
+  name = "ubuntu_vm" 
+  image = "${path.module}/images/jammy-server-cloudimg-amd64.ova" 
+  cpus = 1 
+  memory = "1024 mib" 
+network_adapter { type = "nat" } 
 }
